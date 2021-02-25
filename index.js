5a2ecffe1bdcc884918c8fcccedb4e02e64bcf3c
shyam205 <mastershyam2013@gmail.com>
2021-02-25 19:31:25 +0530

    changes in paragraph tag

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ function toggle() {
     var j = i%2;
     if(j==0){
         toggle.classList.add("navtoggle");
-        console.log(i);
         i++;
     }else{
         toggle.classList.remove("navtoggle");
-        console.log(i);
         i++;
     }
 }
 
+function loading(){
+       
+    var popup = document.getElementById("popup");
+    popup.classList.toggle("popupactive");
+}
